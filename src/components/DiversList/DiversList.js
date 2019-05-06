8ea8d8ba55eeed3204af822606ad9afea53e9bc4
Jason <j.grant.brown@gmail.com>
2019-05-06 12:36:10 -0400

    add details link to go to specfic diver profile

--- src/components/DiversList/DiversList.js
+++ src/components/DiversList/DiversList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
 
-// LAter to get from backend
+// LAter to get from backend and link to details of specfice diver profile
 let divers = [{id:1, firstName: "Joe", lastName: "Smith"},{id:2, firstName: "Mike", lastName: "Johnson"}]
 const listDivers = divers.map((diver)=>
-  <li>{diver.firstName} {diver.lastName} </li>)
+  <li>{diver.firstName} {diver.lastName} <Link to="/profiles">Details</Link></li>
+)
 
 
 class DiversList extends React.Component {
